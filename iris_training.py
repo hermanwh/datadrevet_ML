ff497942d43fed8257f3e92e40d8022c3d69e136
hermanwh <hermanwh@hotmail.com>
2019-09-21 00:39:19 +0200

    Now creates model for each dataframe column, saved as tensorflowjs-file. Deleted old files. Placed models in gitignore

--- iris_training.py
+++ iris_training.py
@@ -20,32 +20,50 @@ df = pd.read_csv('iris.csv')
 df = df.drop('variety', 1)
 columns = len(df.iloc[0,:])
 
-X = df.iloc[:, 0:columns-1]
-y = df.iloc[:, columns-1]
+print(df)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False, stratify=None)
+EPOCHS = 100
+BATCH_SIZE = 10
+ACTIVATION = 'relu'
+LOSS = 'mean_squared_error'
+OPTIMIZER = 'adam'
+METRICS = ['mean_squared_error']
+TEST_SIZE = 0.2
+SHUFFLE = True
+VERBOSE = 0
 
-scaling = MinMaxScaler(feature_range=(-1, 1)).fit(X_train)
-X_train = scaling.transform(X_train)
-X_test = scaling.transform(X_test)
-samples = X_train.shape[0]
+for column in df.columns:
+    print(f"Column: {column}")
+    X = df.drop(column, axis=1)
+    y = df[column]
 
-model = Sequential()
-model.add(Dense(25, input_dim=X_train.shape[1], activation='relu'))
-model.add(Dense(15, activation='relu'))
-model.add(Dense(1, activation='relu'))
-# compile the keras model
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_squared_error'])
-# fit the keras model on the dataset
-model.fit(X_train, y_train, epochs=10, batch_size=10, verbose=1)      # verbose = 1 if you want to inspect every epoch
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, shuffle=SHUFFLE)
 
-model.save('my_model.h5')
-tfjs_target_dir = "C:\\Users\\herma\\Documents\\datadrevet_ML"
-tfjs.converters.save_keras_model(model, tfjs_target_dir)
+    model = Sequential()
+    model.add(Dense(10, input_dim=X_train.shape[1], activation=ACTIVATION))
+    model.add(Dense(5, activation=ACTIVATION))
+    model.add(Dense(1, activation=ACTIVATION))
+    model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS)
+    model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
 
-pred_train = model.predict(X_train)
-pred_test = model.predict(X_test)
-r2_train = r2_score(y_train, pred_train)
-r2_test = r2_score(y_test, pred_test)
-print('R2 score train:', r2_train)
-print('R2 score test:', r2_test)
+    tfjs_target_dir = "tfjs_models\\{}".format(column)
+    tfjs.converters.save_keras_model(model, tfjs_target_dir)
+
+    pred_train = model.predict(X_train)
+    pred_test = model.predict(X_test)
+    r2_train = r2_score(y_train, pred_train)
+    r2_test = r2_score(y_test, pred_test)
+    print('R2 score train:', r2_train)
+    print('R2 score test:', r2_test)
+
+    # Code just for IRIS dataset
+    if column == "sepal.length" or  column == "sepal.width":
+        plt.scatter(X_train.iloc[:, 0], y_train.iloc[:], c='blue')
+        plt.scatter(X_test.iloc[:, 0], y_test, c='orange')
+        plt.show()
+    elif column == "petal.length" or column == "petal.width":
+        plt.scatter(X_train.iloc[:, -1], y_train.iloc[:], c='blue')
+        plt.scatter(X_test.iloc[:, -1], y_test, c='orange')
+        plt.show()
+
+    print("---------------")
