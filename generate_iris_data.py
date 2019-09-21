7258f0537d9fcb3e077f4bf424971c93598adb73
hermanwh <hermanwh@hotmail.com>
2019-09-21 12:26:52 +0200

    added function to generate iris data. Implemented loop for model convergence

---
+++ generate_iris_data.py
@@ -0,0 +1,53 @@
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+import tensorflowjs as tfjs
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+
+def generateAdditionalData(size, spread):
+    sample_sepal_length = np.random.normal(5.00600, 0.35249/spread, size)
+    sample_sepal_width = np.random.normal(3.428000, 0.379064/spread, size)
+    sample_petal_length = np.random.normal(1.462000, 0.173664/spread, size)
+    sample_petal_width = np.random.normal(0.2460003, 0.105386/spread, size)
+
+    hstack1 = np.vstack((sample_sepal_length, 
+                        sample_sepal_width,
+                        sample_petal_length,
+                        sample_petal_width)).T
+
+    sample_sepal_length = np.random.normal(5.936000, 0.516171/spread, size)
+    sample_sepal_width = np.random.normal(2.770000, 0.313798/spread, size)
+    sample_petal_length = np.random.normal(4.260000, 0.469911/spread, size)
+    sample_petal_width = np.random.normal(1.326000, 0.197753/spread, size)
+
+    hstack2 = np.vstack((sample_sepal_length, 
+                        sample_sepal_width,
+                        sample_petal_length,
+                        sample_petal_width)).T
+
+    sample_sepal_length = np.random.normal(6.58800, 0.63588/spread, size)
+    sample_sepal_width = np.random.normal(2.974000, 0.322497/spread, size)
+    sample_petal_length = np.random.normal(5.552000, 0.551895/spread, size)
+    sample_petal_width = np.random.normal(2.02600, 0.27465/spread, size)
+
+    hstack3 = np.vstack((sample_sepal_length, 
+                        sample_sepal_width,
+                        sample_petal_length,
+                        sample_petal_width)).T
+
+    hstack = np.vstack((hstack1, hstack2, hstack3))
+
+    dataset = pd.DataFrame({'sepal.length': hstack[:, 0],
+                            'sepal.width': hstack[:, 1],
+                            'petal.length': hstack[:, 2], 
+                            'petal.width': hstack[:, 3]})
+
+    return dataset
--- iris_training.py
+++ iris_training.py
@@ -11,17 +11,19 @@ import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+import generate_iris_data
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 start_time = time.time()
 
 tf.compat.v1.disable_eager_execution()
 
-# load the dataset
-df = pd.read_csv('iris.csv')
-df = df.drop('variety', 1)
-columns = len(df.iloc[0,:])
+df_iris = pd.read_csv('iris.csv')
+df_iris = df_iris.drop('variety', 1)
+columns = len(df_iris.iloc[0,:])
 
+generatedData = generate_iris_data.generateAdditionalData(100, 2)
+df = pd.concat([df_iris, generatedData])
 print(df)
 
 EPOCHS = 100
@@ -39,33 +41,44 @@ for column in df.columns:
     X = df.drop(column, axis=1)
     y = df[column]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, shuffle=SHUFFLE)
+    r2_train = -100
+    r2_test = -100
 
-    model = Sequential()
-    model.add(Dense(10, input_dim=X_train.shape[1], activation=ACTIVATION))
-    model.add(Dense(5, activation=ACTIVATION))
-    model.add(Dense(1, activation=ACTIVATION))
-    model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS)
-    model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
+    run = 1
+    while (r2_test < 0.5 and run < 11):
+        print(f'Run: {run}')
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, shuffle=SHUFFLE)
+
+        model = Sequential()
+        model.add(Dense(10, input_dim=X_train.shape[1], activation=ACTIVATION))
+        model.add(Dense(5, activation=ACTIVATION))
+        model.add(Dense(1, activation=ACTIVATION))
+
+        model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS)
+        model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
+
+        pred_train = model.predict(X_train)
+        pred_test = model.predict(X_test)
+        r2_train = r2_score(y_train, pred_train)
+        r2_test = r2_score(y_test, pred_test)
+        print('R2 score train:', r2_train)
+        print('R2 score test:', r2_test)
+        run += 1
 
     tfjs_target_dir = "tfjs_models\\{}".format(column)
     tfjs.converters.save_keras_model(model, tfjs_target_dir)
 
-    pred_train = model.predict(X_train)
-    pred_test = model.predict(X_test)
-    r2_train = r2_score(y_train, pred_train)
-    r2_test = r2_score(y_test, pred_test)
-    print('R2 score train:', r2_train)
-    print('R2 score test:', r2_test)
 
     # Code just for IRIS dataset
     if column == "sepal.length" or  column == "sepal.width":
         plt.scatter(X_train.iloc[:, 0], y_train.iloc[:], c='blue')
         plt.scatter(X_test.iloc[:, 0], y_test, c='orange')
+        plt.scatter(X_test.iloc[:, 0], pred_test, c='red')
         plt.show()
     elif column == "petal.length" or column == "petal.width":
         plt.scatter(X_train.iloc[:, -1], y_train.iloc[:], c='blue')
         plt.scatter(X_test.iloc[:, -1], y_test, c='orange')
+        plt.scatter(X_test.iloc[:, -1], pred_test, c='red')
         plt.show()
 
     print("---------------")
