3d1dfd192987405e61caa9cb20f4821aeeee3782
hermanwh <hermanwh@hotmail.com>
2019-09-21 17:00:40 +0200

    Refactored code. Divided into methods.

--- iris_training.py
+++ iris_training.py
@@ -10,66 +10,88 @@ import tensorflowjs as tfjs
 import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
-import time
 import generate_iris_data
-
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-start_time = time.time()
-
-tf.compat.v1.disable_eager_execution()
-
-df_iris = pd.read_csv('iris.csv')
-df_iris = df_iris.drop('variety', 1)
-columns = len(df_iris.iloc[0,:])
-
-generatedData = generate_iris_data.generateAdditionalData(100, 2)
-df = pd.concat([df_iris, generatedData])
-print(df)
+import time
 
 EPOCHS = 100
 BATCH_SIZE = 10
-ACTIVATION = 'relu'
-LOSS = 'mean_squared_error'
-OPTIMIZER = 'adam'
-METRICS = ['mean_squared_error']
 TEST_SIZE = 0.2
 SHUFFLE = True
 VERBOSE = 0
 
-for column in df.columns:
-    print(f"Column: {column}")
-    X = df.drop(column, axis=1)
-    y = df[column]
+ACTIVATION = 'relu'
+LOSS = 'mean_squared_error'
+OPTIMIZER = 'adam'
+METRICS = ['mean_squared_error']
 
-    r2_train = -100
-    r2_test = -100
+MODEL_FOLDER = 'tfjs_models'
 
-    run = 1
-    while (r2_test < 0.5 and run < 11):
-        print(f'Run: {run}')
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, shuffle=SHUFFLE)
+def train(df):
+    print('Training on the following data:')
+    print(df)
+    print('-----------------')
 
-        model = Sequential()
-        model.add(Dense(10, input_dim=X_train.shape[1], activation=ACTIVATION))
-        model.add(Dense(5, activation=ACTIVATION))
-        model.add(Dense(1, activation=ACTIVATION))
+    models = []
+    columns = len(df.iloc[0,:])
+    for column in df.columns:
+        start_time = time.time()
+        print(f"Column: {column}")
+        X = df.drop(column, axis=1)
+        y = df[column]
 
-        model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS)
-        model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
+        r2_train = -100
+        r2_test = -100
+        run = 1
+        while (r2_test < 0.5 and run < 11):
+            print(f'Run {run}')
+            X_train, X_test, y_train, y_test = train_test_split(X,
+                                                                y,
+                                                                test_size=TEST_SIZE,
+                                                                shuffle=SHUFFLE
+                                                                )
 
-        pred_train = model.predict(X_train)
-        pred_test = model.predict(X_test)
-        r2_train = r2_score(y_train, pred_train)
-        r2_test = r2_score(y_test, pred_test)
-        print('R2 score train:', r2_train)
-        print('R2 score test:', r2_test)
-        run += 1
+            model = Sequential()
+            model.add(Dense(10, input_dim=X_train.shape[1], activation=ACTIVATION))
+            model.add(Dense(5, activation=ACTIVATION))
+            model.add(Dense(1, activation=ACTIVATION))
 
-    tfjs_target_dir = "tfjs_models\\{}".format(column)
-    tfjs.converters.save_keras_model(model, tfjs_target_dir)
+            model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS)
+            model.fit(X_train,
+                        y_train,
+                        epochs=EPOCHS,
+                        batch_size=BATCH_SIZE,
+                        verbose=VERBOSE
+                        )
 
+            pred_train = model.predict(X_train)
+            pred_test = model.predict(X_test)
+            r2_train = r2_score(y_train, pred_train)
+            r2_test = r2_score(y_test, pred_test)
+            print('R2 score train:', r2_train)
+            print('R2 score test:', r2_test)
+            run += 1
 
-    # Code just for IRIS dataset
+        if r2_test < 0.5:
+            print(f'Training for {column} was unsuccessful')
+        else:
+            tup = (model, column)
+            models.append(tup)
+            print(f'Training completed in {time.time() - start_time} seconds')
+
+        plotScatter(column, X_train, X_test, y_train, y_test, pred_test)
+        print("---------------")
+
+    return models
+
+def saveModels(models, folder):
+    for model, column in models:
+        print(f'Saving column {column} in {folder}')
+        tfjs_target_dir = "{}\\{}".format(folder, column)
+        tfjs.converters.save_keras_model(model, tfjs_target_dir)
+    return True
+
+# just for IRIS dataset
+def plotScatter(column, X_train, X_test, y_train, y_test, pred_test):
     if column == "sepal.length" or  column == "sepal.width":
         plt.scatter(X_train.iloc[:, 0], y_train.iloc[:], c='blue')
         plt.scatter(X_test.iloc[:, 0], y_test, c='orange')
@@ -81,4 +103,21 @@ for column in df.columns:
         plt.scatter(X_test.iloc[:, -1], pred_test, c='red')
         plt.show()
 
-    print("---------------")
+def main():
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+    tf.compat.v1.disable_eager_execution()
+
+    df_iris = pd.read_csv('iris.csv').drop('variety', 1)
+
+    generatedData = generate_iris_data.generateAdditionalData(100, 2)
+    df = pd.concat([df_iris, generatedData])
+
+    models = train(df)
+    saved = saveModels(models, MODEL_FOLDER)
+
+    if (saved):
+        print(f'All models were saved successfully in folder {MODEL_FOLDER}')
+    else:
+        print(f'Saving was unsuccessful')
+
+main()
